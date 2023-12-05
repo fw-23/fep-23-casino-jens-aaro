6992b68b0b9e0fe2985524ea97d5d1bf6f53eeae
Aaro <salmeaar@arcada.fi>
2023-12-05 10:12:57 +0200

    sten sax påsa funtkioner

--- assets/js/ssp.js
+++ assets/js/ssp.js
@@ -1,3 +1,29 @@
 /* Exempel på JS-fil för specifik sida (ssp-sidan i det här fallet) */
 
 console.log('ssp.js init'); // För att se att skriptet laddats in
+
+const player =document.getElementById("vale")
+
+const houseplay = document.getElementById("houseplay")
+
+const resultshow = document.getElementById("resultat")
+
+const ssp=[
+    {name:"sten",win:"sax",los:"pas"},
+    {name:"sax",win:"pas",los:"sten"},
+    {name:"pas",win:"sten",los:"sax"},
+]
+const result=[
+    "WIN",
+    "Lose",
+    "Tie"
+]
+
+
+
+
+
+
+function start(){
+    result.innerHTML=result[player.selectedIndex]
+}
